785220c1128d2f50e0ec861ae7747564cbc4d191
Esteban Borai <estebanborai@gmail.com>
2022-11-12 14:17:04 -0300

    feat: provide naive `View` struct impl

--- src/view/src/main.rs
+++ src/view/src/main.rs
@@ -1,3 +1,48 @@
+use std::env::{args, Args};
+use std::path::PathBuf;
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct View {
+    /// Path to the file to read
+    path: PathBuf,
+    /// Offset to read file from
+    offset: Option<usize>,
+    /// Limit to stop reading file
+    limit: Option<usize>,
+}
+
+fn parse(args: Args) -> View {
+    if args.len() < 2 {
+        // Faltan Argumentos
+        todo!("Print HELP Text");
+    }
+
+    let mut view = View {
+        path: PathBuf::default(),
+        offset: None,
+        limit: None,
+    };
+
+    for (idx, arg) in args.enumerate() {
+        if idx == 0 {
+            // Path to the current executable is skipped
+            continue;
+        }
+
+        if idx == 1 {
+            // This must be the path to the file to read
+            let path = PathBuf::from_str(&arg).unwrap();
+            view.path = path;
+        }
+    }
+
+    view
+}
+
 fn main() {
-    println!("Hello World!");
+    let arguments = args();
+    let view = parse(arguments);
+
+    println!("{:#?}", view);
 }
